639d58cd82ec9a065db43471e1c43e3ff02d26db
CTRLMarcws <marfilipi@hotmail.com>
2020-07-22 01:57:15 -0300

    database methods included

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,9 @@ const sqlite3 = require("sqlite3").verbose()
 
 const db = new sqlite3.Database("./src/database/database.db")
 
-db.serialize( () => {
+module.exports = db
+
+/* db.serialize( () => {
     
     db.run(`
         CREATE TABLE IF NOT EXISTS places (
@@ -30,8 +32,8 @@ db.serialize( () => {
     `
 
     const values = [
-        "url",
-        "Colectoria",
+        "https://images.unsplash.com/photo-1516992654410-9309d4587e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        "Papersider",
         "Endereço",
         "Endereço2",
         "São Paulo",
@@ -49,7 +51,7 @@ db.serialize( () => {
         console.log(this)
     }
 
-    //db.run( query, values, afterInsertData )
+    db.run( query, values, afterInsertData )
 
     db.all(`SELECT name FROM places`, function(err, rows) {
         if(err){
@@ -60,6 +62,14 @@ db.serialize( () => {
         console.log(rows)
     })
 
-})
+    db.run(`DELETE FROM places WHERE id = ?`, [2], function(err){
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Registro deletado com sucesso!")
+    })
+
+}) */
 
 //banco de dados - sqlite 3 - 0:44:26
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const server = express()
 
+const db = require("./database/db")
+
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views",{
     express: server,
@@ -10,22 +12,75 @@ nunjucks.configure("src/views",{
 //configuração de pasta publica (estatica)
 server.use(express.static("public"))
 
+//habilitar uso de req.body na aplicação
+server.use(express.urlencoded({ extended: true }))
+
 server.get("/",(req, res) => {
     return res.render("index.html")
 })
 
 server.get("/create-point",(req, res) => {
+
+    //console.log(req.query)
+
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+    //console.log(req.body)
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?)
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err){
+
+        if(err){
+            console.log(err)
+            return res.send("Erro no cadastro")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run( query, values, afterInsertData )
+
+})
+
 server.get("/search",(req, res) => {
-    return res.render("search-results.html")
+
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if(err){
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+
 })
 
 server.listen(3000)
-
-
-/*
-    nodemon installed as dev dependeces - nunjucks added - routes added - "template engine added"
-    __dirname = caminho até o diretorio atual, nesse caso acima, esta retornando "ecoleta/src"
-*/
